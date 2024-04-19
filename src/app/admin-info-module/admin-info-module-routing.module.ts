a2adf97dff3a534893b9ee85905f71c5c7b1cbf7
NANI ALLI <itsnani502@gmail.com>
2024-04-19 12:25:40 +0530

    admin info teacher also updated

--- src/app/admin-info-module/admin-info-module-routing.module.ts
+++ src/app/admin-info-module/admin-info-module-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudnetTeacherNavComponent } from './studnet-teacher-nav/studnet-teacher-nav.component';
-import { StudentInfoComponent } from '../student-info/student-info.component';
+import{ StudentInfoComponent} from './student-info/student-info.component'
 import { TeacherInfoComponent } from './teacher-info/teacher-info.component';
-
+TeacherInfoComponent
 const routes: Routes = [
   {path:"",component:StudnetTeacherNavComponent,children:[
     {path:"student",component:StudentInfoComponent},
